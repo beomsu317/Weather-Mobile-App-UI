443182a663454e8043f5307b2c92b1cd91da4916
Beomsu Lee <beomsu317@nhn.com>
2022-02-20 22:50:13 +0900

    change HomeScreen directory

--- app/src/main/java/com/bs/weather_mobile_app_ui/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/bs/weather_mobile_app_ui/presentation/home/HomeScreen.kt
@@ -1,12 +1,10 @@
-package com.bs.weather_mobile_app_ui.presentation.ui.home
+package com.bs.weather_mobile_app_ui.presentation.home
 
 import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -17,12 +15,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bs.weather_mobile_app_ui.R
